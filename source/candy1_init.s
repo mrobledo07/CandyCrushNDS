d56cf82f890b8ed3f737fe8af3dc21929c339fac
Miguel Robledo <miguel.robledo@estudiants.urv.cat>
2022-10-23 14:35:47 +0200

    prog1: tareaA+tareaB sin testear

--- source/candy1_init.s
+++ source/candy1_init.s
@@ -1,8 +1,8 @@
 @;=                                                          	     	=
 @;=== candy1_init.s: rutinas para inicializar la matriz de juego	  ===
 @;=                                                           	    	=
-@;=== Programador tarea 1A: xxx.xxx@estudiants.urv.cat				  ===
-@;=== Programador tarea 1B: yyy.yyy@estudiants.urv.cat				  ===
+@;=== Programador tarea 1A: miguel.robledo@estudiants.urv.cat				  ===
+@;=== Programador tarea 1B: miguel.robledo@estudiants.urv.cat				  ===
 @;=                                                       	        	=
 
 
@@ -56,17 +56,17 @@ inicializa_matriz:
 		mov r1, #0			@; r1 es indice de fila
 		mov r8, #0 			@; r8 es indice de movimiento de posiciones
 		
-		.L_for1:
+		.LFor1:
 			mov r2, #0			@; r2 es indice de columna
-		.L_for2:
+		.LFor2:
 			ldrb r7, [r5, r6] 	@; en r7 se cargara en cada bucle el elemento variable del mapa de configuracion
 			strb r7, [r4, r8]
 			mov r9, r7			@; r9 es el elemento variable del mapa de configuracion
 			and r7, #0x07		@; filtramos los 3 bits bajos
 			cmp r7, #0x00
-			bne .L_fiif
+			bne .Lfiif
 			
-			.L_if:
+			.Lif:
 				mov r0, #7			@; el rango del numero aleatorio será entre 0 y 6
 				bl mod_random
 				cmp r0, #0
@@ -84,17 +84,17 @@ inicializa_matriz:
 				cmp r0, #3
 				beq .Lif
 			
-			.L_fiif:
+			.Lfiif:
 			
 			add r6, #1			@; r6 es el indice de desplazamiento para el mapa de configuracion, augmenta en 1
 			add r8, #1			@; el indice de desplazamiento de la matriz augmenta en 1
 			add r2, #1			@; el indice para el bucle que recorre las columnas augmenta en 1
 			cmp r2, #COLUMNS
-			blo .L_for2
+			blo .LFor2
 			
 			add r1, #1			@; el indice para el bucle de las filas augmenta en 1 
 			cmp r1, #ROWS
-			blo .L_for1
+			blo .LFor1
 			
 		
 		pop {r1-r9, pc}				@;recuperar registros y volver
@@ -121,10 +121,165 @@ inicializa_matriz:
 @;		R0 = dirección base de la matriz de juego
 	.global recombina_elementos
 recombina_elementos:
-		push {lr}
+		push {r1-r10,lr}
+		ldr r5, =mat_recomb1
+		ldr r6, = mat_recomb2
+		mov r4, r0
 		
+		mov r1, #0				@; r1 es indice de filas
+		mov r3, #0				@; r3 es indice de desplazamiento
 		
-		pop {pc}
+		.LFor1:
+			mov r2, #0			@; r2 es indice de columnas
+		.LFor2:
+			ldrb r7, [r4, r3]
+			
+			mov r9, r7, lsr #3	@; bits 4 i 3 (2 bits altos) en r9
+			and r9, #0x03
+			cmp r9, #0			@; si los 2 bits altos son 00, son elementos basicos
+			beq .Lcopiar_elembasicosAceros
+			cmp r9, #1			@; si los 2 bits altos son 01, son gelatinas simples
+			beq .Lcopiar_gelatinassimplesbasicas
+			cmp r9, #2			@; si los 2 bits altos son 10, son gelatinas dobles
+			beq .Lcopiar_gelatinasdoblesbasicas
+			and r8, r7, #0x07
+			cmp r8, #7			@; si los 3 bits bajos son unos, copiaremos cero en mat_recomb1 (dependiendo de otras comprobaciones en la funcion copia_ceros)
+			beq .Lcopiar_ceros
+			cmp r8, #0			@; si los 3 bits bajos son ceros, tambien copiaremos cero en mat_recomb1 (dependiendo de otras comprobaciones en la funcion copia_ceros)
+			bne .Lcopiar_elementosbasicos	@; si los 3 bits bajos no son ni todo ceros ni todo unos, seran elementos basicos(dependiendo de los 2 bits altos)
+			
+			.Lcopiar_ceros:
+				cmp r8, #0			@; si los 3 bits bajos son todos unos, será bloque o hueco
+				bne .Lbloque_hueco
+				cmp r9, #0			@; si los 3 bits bajos son todos ceros y los 2 bits altos son diferentes de 00, asignaremos 0 a matrecomb1
+				bne .Lasignacion_ceros
+				beq .Lcontinue_noasignado_matrecomb1
+				
+				.Lbloque_hueco:
+					cmp r9, #2		@; si los 2 bits altos son 10, no será ni bloque ni hueco, no asignamos 0 a matrecomb1
+					beq copiar_elementosbasicos
+				
+				.Lasignacion_ceros:
+					mov r8, #0
+					strb r8, [r5, r3]
+					b .Lcontinue_asignado	
+						
+				b .Lcontinue_noasignado_matrecomb1	@; en caso que no se haya asignado nada, se asignara el elemento correspondiente en matrecomb1 en la funcion marcada
+			
+			.Lcopiar_elementosbasicos:
+				and r8, r7, #0x07		@; si los 3 bits bajos no son ni todos 0 ni todos 1, ponemos los 2 bits altos a 0 para convertirlo a un elemento basico
+				strb r8, [r5, r3]
+				
+				b .Lcontinue_asignado
+			
+			.Lcopiar_elembasicosAceros:
+				and r8, r7, #0x07
+				cmp r8, #0			
+				beq .Lcontinue_noasignado_matrecomb2
+				cmp r8, #7
+				beq .Lcontinue_noasignado_matrecomb2
+				mov r8, #0
+				strb r8, [r6, r3]	@; si los 3 bits bajos no son ni todos 0 ni todos 1, asignamos 0 a matrecomb2
+			
+				b .Lcontinue_asignado
+			
+			.Lcopiar_gelatinassimplesbasicas:
+				and r8, r7, #0x07
+				cmp r8, #7
+				beq .Lcontinue_noasignado_matrecomb2
+				mov r8, #8
+				strb r8, [r6, r3]	@; si los 3 bits bajos no son todos 1, asignamos la gelatina basica 8  a matrecomb2 
+			
+				b .Lcontinue_asignado
+			
+			.Lcopiar_gelatinasdoblesbasicas:
+				and r8, r7, #0x07
+				cmp r8, #7
+				beq .Lcontinue_noasignado_matrecomb2
+				mov r8, #16
+				strb r8, [r6, r3]   @; si los 3 bits bajos no son todos 1, asignamos la gelatina basica 16 a matrecomb2
+			
+				b .Lcontinue_asignado
+				
+			.Lcontinue_noasignado_matrecomb1:
+				strb r7, [r5, r3]
+				b .Lcontinue_asignado
+				
+			.Lcontinue_noasignado_matrecomb2:
+				strb r7, [r6, r3]
+				
+			.Lcontinue_asignado:
+				add r3, #1
+				add r2, #1
+				cmp r2, #COLUMNS
+				blo .LFor2
+				add r1, #1
+				cmp r1, #ROWS
+				blo .LFor1
+				
+				
+		@; segunda parte
+		
+		mov r1, #0		@; indice de filas
+		mov r10, #0		@; indice de desplazamiento
+		mul r0, #COLUMNS, #ROWS	@; cargamos en r0 el rango para generar un numero aleatorio
+		
+		.LFor3:
+			mov r2, #0	@; indice de columnas
+		.LFor4:
+			ldrb r7, [r4, r10]		@; cargamos en r7 el valor contenido en la posicion "i" de la matriz
+			and r7, #0x07
+			cmp r7, #0
+			beq .LFinal				@; si el valor de r7 es 0, 8 o 16, ignoramos la actual posicion
+			cmp r7, #7
+			beq .LFinal				@; si el valor de r7 es 0,8,16, hueco o bloque solido, ignoramos la posicion
+			
+			b .Lcodigo_elemento
+			
+			.Lhay_secuencia:
+				strb r9, [r6, r10]	@; restituimos el valor de la posicion actual de mat_recomb2
+			
+			.Lcodigo_elemento:
+				bl mod_random()
+				mov r7, r0			@; posicion aleatoria de mat_recomb1 en r7
+				ldrb r8, [r5, r0]	@; cargamos el valor de una posicion aleatoria de mat_recomb1 en r8
+				cmp r8, #0			@; si el valor que hemos cargado es 0, repetimos, hasta que no sea 0
+				beq .Lcodigo_elemento
+				
+			
+			ldrb r9, [r6, r10]		@; cargamos en r9 el valor de la posicion actual de mat_recomb2
+			and r3, r9, #0x07
+			cmp r3, #0				@; si es una gelatina basica 0, 8 o 16, mat_recomb2[i] = mat_recomb2[i]+mat_recomb1[aleatorio]
+			addeq r8, r9
+			strb r8, [r6, r10]		@; si no es una gelatina basica, mat_recomb2[i] = mat_recomb1[aleatorio]
+			
+			.Lcuenta_repeticiones:
+				mov r0, r4
+				mov r3, #2			@; orientacion = oeste
+				bl cuenta_repeticiones
+				cmp r0, #3			
+				beq .Lhay_secuencia
+				mov r0, r4
+				mov r3, #3			@; orientacion = norte
+				bl cuenta_repeticiones
+				cmp r0, #3
+				beq .Lhay_secuencia	@; si hay secuencia, restituimos el valor anterior en la posicion actual de mat_recomb2, y volvemos a coger una pos. aleatoria de mat_recomb1
+			
+				mov r3, #0
+				strb r3, [r5, r7]	@; fijamos a 0 el valor que hemos usado de mat_recomb1
+				strb r8, [r4, r10] 	@; asignamos el valor de mat_recomb2[i] en la matriz_de_juego[i]
+		
+		.LFinal:
+			add r10, #1
+			add r2, #1
+			cmp r2, #COLUMNS
+			blo .LFor4
+			add r1, #1
+			cmp r1, #ROWS
+			blo .LFor3
+			
+	
+		pop {r1-r10,pc}
 
 
 
