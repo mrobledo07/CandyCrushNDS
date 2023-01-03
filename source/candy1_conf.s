54249a73a80fba4c11b66c9cc766ee3b119d7ee6
Miguel Robledo <miguel.robledo@estudiants.urv.cat>
2023-01-03 21:49:01 +0100

    res.confl.2

--- source/candy1_conf.s
+++ source/candy1_conf.s
@@ -1,7 +1,7 @@
 @;=                                                        				=
-@;=== candy1_conf: variables globales de configuraciÛn del juego  	  ===
+@;=== candy1_conf: variables globales de configuraci√≥n del juego  	  ===
 @;=                                                       	        	=
-@;=== autor: Santiago RomanÌ 	(2014-08-20)					  	  ===
+@;=== autor: Santiago Roman√≠ 	(2014-08-20)					  	  ===
 @;=                                                       	        	=
 
 
@@ -10,10 +10,10 @@
 		.align 2
 
 
-@; lÌmites de movimientos para cada nivel;
-@;	los lÌmites corresponder·n a los niveles 0, 1, 2, ..., hasta MAXLEVEL-1
-@;								(MAXLEVEL est· definida en "include/candy1.h")
-@;	cada lÌmite debe ser un n˙mero entre 3 y 99.
+@; l√≠mites de movimientos para cada nivel;
+@;	los l√≠mites corresponder√°n a los niveles 0, 1, 2, ..., hasta MAXLEVEL-1
+@;								(MAXLEVEL est√° definida en "include/candy1.h")
+@;	cada l√≠mite debe ser un n√∫mero entre 3 y 99.
 		.global max_mov
 	max_mov:	.byte 20, 27, 31, 45, 52, 32, 21, 90, 50 
 
@@ -21,7 +21,7 @@
 @; objetivo de puntos para cada nivel;
 @;	si el objetivo es cero, se supone que existe otro reto para superar el
 @;	nivel, por ejemplo, romper todas las gelatinas.
-@;	el objetivo de puntos debe ser un n˙mero menor que cero, que se ir·
+@;	el objetivo de puntos debe ser un n√∫mero menor que cero, que se ir√°
 @;	incrementando a medida que se rompan elementos.
 		.align 2
 		.global pun_obj
@@ -29,18 +29,29 @@
 
 
 
-@; mapas de configuraciÛn de la matriz;
-@;	cada mapa debe contener tantos n˙meros como posiciones tiene la matriz,
+@; mapas de configuraci√≥n de la matriz;
+@;	cada mapa debe contener tantos n√∫meros como posiciones tiene la matriz,
 @;	con el siguiente significado para cada posicion:
-@;		0:		posiciÛn vacÌa (a rellenar con valor aleatorio)
+@;		0:		posici√≥n vac√≠a (a rellenar con valor aleatorio)
 @;		1-6:	elemento concreto
-@;		7:		bloque sÛlido (irrompible)
-@;		8+:		gelatinas simple (a sumarle cÛdigo de elemento)
-@;		16+:	gelatina doble (a sumarle cÛdigo de elemento)
+@;		7:		bloque s√≥lido (irrompible)
+@;		8+:		gelatinas simple (a sumarle c√≥digo de elemento)
+@;		16+:	gelatina doble (a sumarle c√≥digo de elemento)
 		.global mapas
-	mapas:
-
-	@; mapa 0: todo aleatorio
+	mapas: 
+	
+	@; mapa 0: secuencia
+		.byte 0,0,0,0,0,0,0,0,0
+		.byte 0,0,0,0,0,0,0,0,0
+		.byte 0,0,0,0,0,0,0,0,0
+		.byte 0,0,0,0,0,0,0,0,0
+		.byte 0,0,0,0,0,0,0,0,0
+		.byte 0,0,0,0,0,0,0,0,0
+		.byte 0,0,0,0,0,0,0,0,0
+		.byte 0,0,0,0,0,0,0,0,0
+		.byte 0,0,0,3,3,3,3,3,3
+	
+	@; mapa 1: todo 0
 		.byte 0,0,0,0,0,0,0,0,0
 		.byte 0,0,0,0,0,0,0,0,0
 		.byte 0,0,0,0,0,0,0,0,0
@@ -51,7 +62,7 @@
 		.byte 0,0,0,0,0,0,0,0,0
 		.byte 0,0,0,0,0,0,0,0,0
 
-	@; mapa 1: paredes horizontales y verticales
+	@; mapa 2: paredes horizontales y verticales
 		.byte 0,0,0,0,0,0,0,0,0
 		.byte 0,0,0,0,0,0,0,0,0
 		.byte 0,7,7,7,7,7,0,0,0
@@ -62,7 +73,7 @@
 		.byte 0,0,7,7,7,7,0,0,7
 		.byte 0,0,0,0,0,0,0,0,7
 
-	@; mapa 2: huecos y bloques sÛlidos
+	@; mapa 3: huecos y bloques s√≥lidos
 		.byte 15,15,7,15,0,0,0,0,0
 		.byte 0,15,15,7,15,0,0,0,15
 		.byte 0,0,0,0,0,15,0,0,15
@@ -73,7 +84,7 @@
 		.byte 0,0,0,0,0,15,0,0,0
 		.byte 0,0,0,0,0,0,0,0,15
 	
-	@; mapa 3: gelatinas simples
+	@; mapa 4: gelatinas simples
 		.byte 0,0,0,8,8,8,0,0,15
 		.byte 0,0,0,0,8,0,0,0,15
 		.byte 0,0,8,8,8,8,0,0,15
@@ -83,8 +94,8 @@
 		.byte 0,0,8,8,8,8,0,0,15
 		.byte 0,0,0,0,0,0,0,0,15
 		.byte 0,0,0,0,0,0,0,0,15
-
-	@; mapa 4: gelatinas dobles
+	
+	@; mapa 5: gelatinas dobles
 		.byte 0,15,0,15,0,7,0,15,15
 		.byte 0,0,7,0,0,7,0,0,15
 		.byte 10,3,8,1,1,8,3,3,0
@@ -94,8 +105,8 @@
 		.byte 2,3,15,0,0,16,0,0,15
 		.byte 0,0,8,0,0,8,0,0,0
 		.byte 0,4,7,0,0,7,0,0,15
-
-	@; mapa 5: combinaciones en horizontal de 3, 4 y 5 elementos
+	
+	@; mapa 6: combinaciones en horizontal de 3, 4 y 5 elementos
 		.byte 1,1,1,15,2,2,2,2,7
 		.byte 3,3,3,3,3,15,7,7,15
 		.byte 4,1,4,4,4,4,15,7,15
@@ -106,7 +117,7 @@
 		.byte 7,7,7,15,7,7,7,15,15
 		.byte 15,15,7,15,15,15,7,15,15
 
-	@; mapa 6: combinaciones en vertical de 3, 4 y 5 elementos
+	@; mapa 7: combinaciones en vertical de 3, 4 y 5 elementos
 		.byte 1,3,4,1,5,6,2,15,15
 		.byte 1,3,1,4,2,5,7,15,15
 		.byte 1,3,4,4,2,5,15,7,15
@@ -116,8 +127,8 @@
 		.byte 2,7,15,6,6,5,6,7,7
 		.byte 7,15,15,7,7,5,6,7,15
 		.byte 15,15,7,15,15,5,7,15,15
-
-	@; mapa 7: combinaciones cruzadas (hor/ver) de 5, 6 y 7 elementos
+		
+	@; mapa 8: combinaciones cruzadas (hor/ver) de 5, 6 y 7 elementos
 		.byte 15,15,7,15,15,7,15,15,15
 		.byte 1,2,3,3,4,3,7,0,15
 		.byte 1,2,7,5,3,7,7,0,15
@@ -128,7 +139,7 @@
 		.byte 7,8,1,5,4,6,8,0,15
 		.byte 8,8,8,8,8,8,8,0,15
 		
-	@; mapa 8: no hay combinaciones ni secuencias
+	@; mapa 9: no hay combinaciones ni secuencias
 		.byte 15,15,15,15,15,15,15,15,15
 		.byte 15,15,15,15,15,15,15,15,15
 		.byte 1,2,3,3,7,3,15,15,15
@@ -139,8 +150,18 @@
 		.byte 12,5,4,3,11,5,15,15,15
 		.byte 7,7,17,19,4,6,15,15,15
 
+	@; mapa 10: todo elementos (la matriz recombinada ser√° diferente)
+		.byte 17,5,9,10,4,2,3,1,6
+		.byte 18,21,4,5,11,14,1,4,3
+		.byte 1,10,6,20,3,2,4,5,17
+		.byte 5,6,3,2,1,17,18,19,20
+		.byte 17,5,9,10,4,2,3,1,6
+		.byte 1,10,6,20,3,2,4,5,17
+		.byte 17,5,9,10,4,2,3,1,6
+		.byte 5,6,3,2,1,17,18,19,20
+		.byte 17,5,9,10,4,2,3,1,6
+		
 
 
 
 .end
-	
