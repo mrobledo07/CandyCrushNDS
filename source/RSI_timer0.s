dc82b2cabfe39261a5f15f44cd214b89a618c691
Miguel Robledo <miguel.robledo@estudiants.urv.cat>
2023-01-23 15:26:31 +0100

    prog1: tareas2A+2E sin testear

--- source/RSI_timer0.s
+++ source/RSI_timer0.s
@@ -121,52 +121,75 @@ desactiva_timer0:
 @;  el efecto de aceleración (con un límite).
 	.global rsi_timer0
 rsi_timer0:
-		push {lr}
+		push {r0-r7, lr}
 		ldr r0, =vect_elem		@; r0 = dirección base del array vect_elem
-		mov r1, #0				@; r1 = 0 (índice para recorrer el array vect_elem)
-		ldr r2, =n_sprites		
-		ldr r2, [r2]			@; r2 = n_sprites (límite del array)
+		mov r7, #0				@; r7 = 0 (índice para recorrer el array vect_elem)
+		ldr r6, =n_sprites		
+		ldr r6, [r6]			@; r6 = n_sprites (límite del array)
+		ldr r3, =divFreq0
+		ldrh r5, [r3]
+		mov r5, r5, lsr #32		@; r5 = r5/2^32		(límite de aceleración del sprite)
 		
 		.Lbucle
 		ldrb r3, [r0]
 		cmp r3, #0
-		ble .LfiBucle
-		sub r3, #1
-		strb r3, [r0]
+		ble .LfiBucle			@; if(r3 <= 0) b.LfiBucle
+		sub r3, #1				@; r3--
+		strb r3, [r0]			@; ii = r3 (se decrementa el valor del atributo ii)
 		ldrb r3, [r0, #3]		@; r3 = atributo vx del elemento
 		cmp r3, #0
-		bne .LmoverHorizontal
+		bne .LmoverHorizontal	@; if(r3 != 0) b .LmoverHorizontal (se moverá en diagonal)
 		ldrb r3, [r0, #4]		@; r3 = atributo vy del elemento
 		cmp r3, #0
-		bne .LmoverVertical
-		b .LfiBucle
+		bne .LmoverVertical		@; if(r3 != 0) b .LmoverVertical
+		b .LfiBucle	
 		
 		.LmoverHorizontal:
 		ldrb r4, [r0, #1]		@; r4 = atributo px del elemento
-		add r4, r3
-		strb r4, [r0, #1]
-		b .LfiBucle
+		add r4, r3				@; r4 = px+ vx
+		strb r4, [r0, #1]		@; vect_elem.px = r4
+		@; si algún elemento se mueve en horizontal, después se mueve en vertical
+		@; ya que un elemento no puede moverse solo en horizontal, sino en diagonal o en vertical
+		ldrb r3, [r0, #4]		@; r3 = atributo vy del elemento						
 		
 		.LmoverVertical:
 		ldrb r4, [r0, #2]		@; r4 = atributo py del elemento
-		add r4, r3
-		strb r4, [r0, #2]
+		add r4, r3				@; r4 = py + vy
+		strb r4, [r0, #2]		@; vect_elem.py = r4
 	
+		ldrb r1, [r0, #1]		@; r1 = atributo px (actualizado/nuevo) del elemento	
+		ldrb r2, [r0, #2]		@; r2 = atributo py (actualizado/nuevo) del elemento
+		bl SPR_moverSprite()
+		
 		ldr r3, =update_spr
 		mov r4, #1
-		strh r4, [r3]
+		strh r4, [r3]			@; update_spr = 1 (ya que se ha actualizado un sprite)
 		
 		ldr r3, =divF0
-		ldrh r4, [r3]
+		ldrh r4, [r3]			@; r4 = divF0
 		mov r4, r4, lsr #1		@; r4 = r4/2 (disminuye el divisor de frecuencia en un factor de 2)
+		cmp r4, r5
+		movhi r4, r5			@; if(r4 > r5) r4 = r5 (si se ha superado el límite de aceleración, r4 se establece en el límite)
+		strh r4, [r3]			@; divF0 = divF0/2 (para conseguir el efecto de aceleración)
 		
 		
 		.LfiBucle:
-			
+		add r7, #1
+		cmp r7, r6			
+		beq .Lfinal			@; if (i == n_sprites) b .Lfinal	(se ha recorrido todo el array)
+		mov r1, #1
+		add r0, r1, lsl #2
+		add r0, r1			@; r0 = r0 + 5 (así nos posicionamos en la siguiente posición del array vect_elem el cual en cada posición tiene 5 atributos de 1 byte cada uno)
+		b .Lbucle
 		
+		.Lfinal:
 		
+		ldr r3, =update_spr
+		ldrh r4, [r3]
+		cmp r4, #0
+		beq desactiva_timer0	 @; if (update_spr == 0) b desactiva_timer0
 		
-		pop {pc}
+		pop {r0-r7, pc}
 
 
 
