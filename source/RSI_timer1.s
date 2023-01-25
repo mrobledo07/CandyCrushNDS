a560cf2ee110b924fc296d3a4decf2556c57b540
Alex Casanova <alex.casanova@estudiants.urv.cat>
2023-01-25 18:35:27 +0100

    2B i 2F acabades, sense provar

--- source/RSI_timer1.s
+++ source/RSI_timer1.s
@@ -1,7 +1,7 @@
 @;=                                                          	     	=
 @;=== RSI_timer1.s: rutinas para escalar los elementos (sprites)	  ===
 @;=                                                           	    	=
-@;=== Programador tarea 2F: xxx.xxx@estudiants.urv.cat				  ===
+@;=== Programador tarea 2F: alex.casanova@estudiants.urv.cat				  ===
 @;=                                                       	        	=
 
 .include "../include/candy2_incl.i"
@@ -12,7 +12,7 @@
 		.align 2
 		.global timer1_on
 	timer1_on:	.hword	0 			@;1 -> timer1 en marcha, 0 -> apagado
-	divFreq1: .hword	?			@;divisor de frecuencia para timer 1
+	divFreq1: .hword	-5727,5			@;divisor de frecuencia para timer 1
 
 
 @;-- .bss. variables (globales) no inicializadas ---
@@ -36,20 +36,56 @@
 @;		R0 = init;  valor a trasladar a la variable 'escSen' (0/1)
 	.global activa_timer1
 activa_timer1:
-		push {lr}
+		push {r1-r3, lr}
+		ldr r1, =escNum
+		mov r2, #0
+		strh r2, [r1]                 @; escNum = 0
 		
+		ldr r1, =timer1_on
+		mov r2, #1
+		strh r2, [r1]                 @; timer1_on = 1
 		
-		pop {pc}
+		ldr r1, =0x04000104           @; TIMER1_DATA
+		ldr r2, =divFreq1
+		ldrh r3, [r2]
+		strh r3, [r1]                 @; Divisor de freqüència a TIMER1_DATA
+		
+		ldr r1, =0x04000106           @; TIMER1_CR   
+		ldrh r2, [r1]
+		orr r2, #0x00C1               @; Màscara per iniciar el timer amb interrupcions activades, sense enllaçar amb el timer anterior i amb una freq d'entrada F/64
+		strh r2, [r1]
+		
+		ldr r1, =escSen
+		strh r0, [r1]                 @; escSen = init
+		
+		cmp r0, #0
+		bne .LFinal
+		
+		mov r1, #1                   
+		mov r2, r1, lsl #8            @; 1,0 amb format coma fixa 0,8,8; py (SPR_fijarEscalado) = 1,0
+		ldr r1, =escFac               
+		strh r2, [r1]                 @; escFac = 1,0
+		mov r0, #0                    @; igrp (SPR_fijarEscalado) = 0
+		mov r1, r2                    @; px (SPR_fijarEscalado) = escFac (1,0)
+		bl SPR_fijarEscalado
+		
+		.LFinal:
+		pop {r1-r3, pc}
 
 
 @;TAREA 2Fc;
 @;desactiva_timer1(); rutina para desactivar el timer 1.
 	.global desactiva_timer1
 desactiva_timer1:
-		push {lr}
-		
-		
-		pop {pc}
+		push {r0, r1, lr}
+		ldr r0, =timer1_on
+		mov r1, #0                    @; Timer1_on = 0
+		strh  r1, [r0]
+		ldr r0, =0x04000106           @; TIMER1_CR
+		ldrh r1, [r0]
+		bic r1, #128                  @; Bit 7 a 0, s'atura el timer
+		strh r1, [r0]
+		pop {r0, r1, pc}
 
 
 
@@ -60,10 +96,39 @@ desactiva_timer1:
 @;	se desactivará el timer1.
 	.global rsi_timer1
 rsi_timer1:
-		push {lr}
+		push {r0-r2, lr}
+		ldr r0, =escNum
+		ldrh r1, [r0]
+		add r1, #1                     @; escNum++
+		strh r1, [r0]
+		cmp r1, #32 
+		bne .LContinua                 @; Si escNum = 32 es desactiva el timer i es surt de la rutina
 		
+		bl desactiva_timer1
+		b .LFinal2
 		
-		pop {pc}
+		.LContinua:
+		ldr r0, =escSen
+		ldrh r1, [r0]
+		ldr r0, =escFac
+		ldrh r2, [r0]
+		
+		cmp r1, #0                      @; escSen = 0, escFac decrementa; escSen = 1, escFac incrementa
+		subeq r2, #32
+		addne r2, #32
+		
+		strh r2, [r0]
+		
+		mov r0, #0                      @; igrp (SPR_fijarEscalado) = 0
+		mov r1, r2                      @; px (SPR_fijarEscalado) = escFac = py
+		bl SPR_fijarEscalado
+		
+		ldr r0, =update_spr
+		mov r1, #1
+		strh r1, [r0]                   @; update_spr = 1
+		
+		.LFinal2:
+		pop {r0-r2, pc}
 
 
 
